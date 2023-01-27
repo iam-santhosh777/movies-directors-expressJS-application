a6e6b02b07c355712766ab6976c635cdbdbd2e39
santhosh kumar <thisis.santhosh777@gmail.com>
2023-01-27 18:29:40 +0000

    CREATE a movie in movie table

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const sqlite3 = require("sqlite3");
 const app = express();
 const path = require("path");
 const dbPath = path.join(__dirname, "moviesData.db");
+app.use(express.json());
 
 let db = null;
 
@@ -23,7 +24,7 @@ const initializeDBAndServer = async () => {
 };
 initializeDBAndServer();
 
-// API - 1: GET Movie's from Database
+// API - 1: GET the list of all the movies from movie table.
 
 const convertDbObject = (objectItem) => {
   return {
@@ -37,4 +38,19 @@ app.get("/movies/", async (request, response) => {
         movie;`;
   const moviesList = await db.all(getMoviesQuery);
   response.send(moviesList.map((eachMovie) => convertDbObject(eachMovie)));
+});
+
+// API - 2: POST (or) CREATE a movie in movie table
+
+app.post("/movies/", async (request, response) => {
+  const { directorId, movieName, leadActor } = request.body;
+  const addMovieQuery = `
+  INSERT INTO movie(director_id, movie_name, lead_actor)
+  VALUES(
+      ${directorId},
+      '${movieName}',
+      '${leadActor}'
+  );`;
+  await db.run(addMovieQuery);
+  response.send("Movie Addded Successfully");
 });
